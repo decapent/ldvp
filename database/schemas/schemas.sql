c2c077aab9ce173e61270299ac3c0f9c88f80f2e
Patrick Lavallee <plavallee@carebook.com>
2022-05-12 20:50:33 -0400

    corrected schema creation script

--- database/schemas/schemas.sql
+++ database/schemas/schemas.sql
@@ -1,14 +1,14 @@
 IF (NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'ldvp')) 
 BEGIN
-    CREATE SCHEMA [ldvp] AUTHORIZATION [dbo]
+    EXEC ('CREATE SCHEMA [ldvp] AUTHORIZATION [dbo]')
 END
 
 IF (NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'saison2')) 
 BEGIN
-    CREATE SCHEMA [saison2] AUTHORIZATION [dbo]
+    EXEC ('CREATE SCHEMA [saison2] AUTHORIZATION [dbo]')
 END
 
 IF (NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'saison3')) 
 BEGIN
-    CREATE SCHEMA [saison3] AUTHORIZATION [dbo]
+    EXEC ('CREATE SCHEMA [saison3] AUTHORIZATION [dbo]')
 END
